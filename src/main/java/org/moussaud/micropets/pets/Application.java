1061a71216cd18385a7c992012f7c9b5333b82f1
Benoit Moussaud <benoit@moussaud.org>
2023-03-21 10:43:57 +0100

    fix Application

--- src/main/java/org/moussaud/micropets/pets/Application.java
+++ src/main/java/org/moussaud/micropets/pets/Application.java
@@ -2,6 +2,9 @@ package org.moussaud.micropets.pets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +12,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public HttpExchangeRepository htttpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
+
 }
